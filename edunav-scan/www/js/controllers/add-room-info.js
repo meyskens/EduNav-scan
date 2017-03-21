62a550cd8bacee4e9af823dd694c299d08b0b392
Maartje Eyskens <meyskens@me.com>
2017-03-21 09:24:17 +0100

    Add add-room-info

--- edunav-scan/www/js/controllers/add-room-info.js
+++ edunav-scan/www/js/controllers/add-room-info.js
@@ -1,7 +1,28 @@
 angular.module('edunavscan.controllers').controller('AddRoomInfoCtrl', addRoomInfoController)
 
-addRoomInfoController.$inject = ["$scope", "BackendService", "$ionicLoading", "$stateParams", "$state"];
+addRoomInfoController.$inject = ["$scope", "BackendService", "$ionicLoading", "$stateParams", "$state", "$ionicPopup", "$ionicHistory"];
 
-function addRoomInfoController($scope, BackendService, $ionicLoading, $stateParams, $state) {
-	
+function addRoomInfoController($scope, BackendService, $ionicLoading, $stateParams, $state, $ionicPopup, $ionicHistory) {
+    $scope.roomInfo = { name: "", comment:"" }
+	$scope.save = function() {
+        if (!$scope.roomInfo.name) {
+            console.log($scope.roomInfo.name)
+            console.log($scope.roomInfo.comment)
+            return $ionicPopup.alert({
+                title: "Error",
+                template: "You need to fill in a name"
+            })
+        }
+        $ionicLoading.show({
+            template: '<ion-spinner></ion-spinner>',
+            duration: 3000
+        })
+        BackendService.addRoom($stateParams.mapID, $stateParams.x, $stateParams.y, $scope.roomInfo.name, $scope.roomInfo.comment).then((response) => {
+            $ionicLoading.hide()
+            $ionicHistory.nextViewOptions({
+                disableBack: true
+            });
+            $state.go("app.addRoom")
+        })
+    }
 }
--- edunav-scan/www/js/services/backend.js
+++ edunav-scan/www/js/services/backend.js
@@ -11,5 +11,22 @@ function backendService($http) {
         return $http.get("https://edunav.eyskens.me/maps/" + id);
     }
 
+    this.addRoom = function(mapID, x, y, name, comment) {
+        var keysURL = "keys.json"
+        if(ionic.Platform.isAndroid()){
+            keysURL = "/android_asset/www/" + keysURL
+        }
+        return $http.get(keysURL).success(function(keys){
+            $http.post("https://edunav.eyskens.me/rooms/" + keys.api + "/add", {
+                mapID: mapID,
+                x: parseFloat(x),
+                y: parseFloat(y),
+                name: name,
+                comment: comment
+            });
+        });
+        
+    }
+
     return this
 }
